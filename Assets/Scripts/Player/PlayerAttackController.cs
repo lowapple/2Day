f93e8300101729bf52725a3164311f71cdf4e0ab
Junseo Youn <plancatlog@gmail.com>
2017-07-10 21:21:37 +0900

    Add HumanMale Animation, Animator, Animation Avatar

---
+++ Assets/Scripts/Player/PlayerAttackController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackController : MonoBehaviour {
+
+	public Animator playerAnimator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			playerAnimator.SetTrigger("Attack");
+			playerAnimator.SetFloat("AttackCount", 0.0f);
+		}
+		if(Input.GetMouseButtonDown(1)){
+			Debug.Log("Right");
+		}		
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+}
